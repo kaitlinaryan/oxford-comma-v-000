6d8b459347e99b0133950062187c96d8dce2969e
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-05 19:24:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,8 @@ if array.length < 2
   array.join
  elsif array.length == 2
   return "#{array[0]} and #{array[1]}"
-
+else array.length > 2
+  array[-1].insert(0, "and ")
+  array.join(", ")
 end
 end
