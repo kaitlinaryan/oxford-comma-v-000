f45d3f33acdb2e1de827247e6c99cac2d0581ebb
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-05 19:23:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,8 @@
 def oxford_comma(array)
-if array.length == 2
- return "#{array[0]} and #{array[1]}"
- end
+if array.length < 2
+  array.join
+ elsif array.length == 2
+  return "#{array[0]} and #{array[1]}"
+
+end
 end
